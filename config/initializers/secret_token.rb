1c50e119833a51b414c67d819d4f4e266fe56c46
TKitamura <t.kitamura@tbs-v.co.jp>
2011-10-04 18:56:20 +0900

    first commit/ users and sessions authenticate/signin/out/up"

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Nozaman::Application.config.secret_token = '657e64ad937d66d3ec4a4af15f4f9cf4cf763cc4d3c8a22a8644c8552104982c6a8350aa01d638cc3e6fe3f609371f2decb676d3b67c1945803e287bb2835e02'
